bd527ce85e59d209894293c359f91d7312dc86c0
itsf4llofstars <irooted4hal@mailfence.com>
2023-09-04 23:54:48 -0500

    Initial commit (ssl)
    Started after adding the index.html and greet.html templates
    We are now just talking about isitchristmas.com at 35:00+ minutes

---
+++ lecture3/hello/urls.py
@@ -0,0 +1,14 @@
+try:
+    from django.urls import path
+
+    from . import views
+except ImportError:
+    pass
+
+urlpatterns = {
+    path("", views.index, name="index"),
+    path("harley", views.harley, name="harley"),
+    path("sister", views.sister, name="sister"),
+    path("brother", views.brother, name="brother"),
+    path("<str:name>", views.greet, name="greet"),
+}
