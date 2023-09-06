f8c20f513e7974b2abfe7f2c609dfd70db76e27a
itsf4llofstars <irooted4hal@mailfence.com>
2023-09-05 19:59:52 -0500

    Add tasks global, add index funct
    Task variable is a list of string tasks todo
    
    index func return a render of the index.html that
    will display the tasks variable

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+# Global that which will be avail to everything in
+# the project
+tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
+def index(request):
+    # template to be rendered
+    return render(request, "tasks/index.html", {
+        # Information that index.html needs
+        "tasks": tasks
+    }
+)
