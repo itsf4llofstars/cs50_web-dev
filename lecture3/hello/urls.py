3361c08e374a2dc3360abbd93c4f4c38858958f1
itsf4llofstars <irooted4hal@mailfence.com>
2023-09-07 23:29:02 -0500

    Change urlpatterns variable braces to brackets

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path("", views.index, name="index"),
     path("harley", views.harley, name="harley"),
     path("sister", views.sister, name="sister"),
     path("brother", views.brother, name="brother"),
     path("<str:name>", views.greet, name="greet"),
-}
+]
